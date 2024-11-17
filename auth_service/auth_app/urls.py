7d17ef037121e661cd4e1564a84fbd859a6af756
Aleksei Konstantinov <mralexey111@gmail.com>
2024-11-17 16:46:50 +0300

    Add User REST API to urls

--- auth_service/auth_app/urls.py
+++ auth_service/auth_app/urls.py
@@ -1,14 +1,22 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
+from .views import UserCreateView, UserViewSet
+
 app_name = 'auth_app'
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', UserCreateView.as_view(), name='register'),
+    path('api/', include(router.urls)),
 ]
