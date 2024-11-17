3b31a95371297c2e9266221325d6dbdd48077126
Aleksei Konstantinov <mralexey111@gmail.com>
2024-11-17 16:48:08 +0300

    Add Profile REST API to urls

--- profiles_service/profiles_service/urls.py
+++ profiles_service/profiles_service/urls.py
@@ -2,8 +2,9 @@
 URL configuration for profiles_service project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('profiles/', include('profiles.urls')),
 ]
