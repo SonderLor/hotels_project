856313ff4796df14a3e5723accf713feda78d6a7
Aleksei Konstantinov <mralexey111@gmail.com>
2024-11-23 22:39:16 +0300

    Fix

--- auth_service/auth_service/settings.py
+++ auth_service/auth_service/settings.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'auth_app.middlewares.CurrentUserMiddleware',
 ]
 
 ROOT_URLCONF = 'auth_service.urls'
