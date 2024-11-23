b623a38cedfe6c4bf240db82c564782e0d818de5
Aleksei Konstantinov <mralexey111@gmail.com>
2024-11-23 22:38:53 +0300

    Add function to define path for image field

--- profiles_service/profiles/models.py
+++ profiles_service/profiles/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+def profile_picture_upload_path(instance, filename):
+    return f"profile_pictures/{instance.id}.png"
+
+
 class Profile(models.Model):
     user_id = models.BigIntegerField(unique=True)
     email = models.EmailField(unique=True)
@@ -8,7 +12,7 @@ class Profile(models.Model):
     bio = models.TextField(blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to=profile_picture_upload_path, blank=True, null=True)
 
     def __str__(self):
         return f"Profile for User ID {self.user_id}"
