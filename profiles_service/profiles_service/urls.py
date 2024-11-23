fa214cb6a4610e488a4ae5a2164f8f1446cb5a44
Aleksei Konstantinov <mralexey111@gmail.com>
2024-11-23 22:39:42 +0300

    Add static urls

--- profiles_service/profiles_service/urls.py
+++ profiles_service/profiles_service/urls.py
@@ -1,10 +1,12 @@
 """
 URL configuration for profiles_service project.
 """
+from django.conf.urls.static import static
+from profiles_service import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
