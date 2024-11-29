72894136c9e7ecf974f0d8ada1713469bdca351a
Aleksei Konstantinov <mralexey111@gmail.com>
2024-11-29 13:03:59 +0300

    Add API urls configuration

--- hotels_service/hotels_service/urls.py
+++ hotels_service/hotels_service/urls.py
@@ -1,9 +1,13 @@
 """
 URL configuration for hotels_service project.
 """
+from django.conf.urls.static import static
+from hotels_service import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/hotels-rooms/hotels-app/', include('hotels.urls')),
+    path('api/hotels-rooms/rooms-app/', include('rooms.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
