fa997f46eac23c64c21ad70760d250c29f62f169
Aleksei Konstantinov <mralexey111@gmail.com>
2024-11-29 12:49:42 +0300

    Add roles to JWT

--- auth_service/auth_app/serializers.py
+++ auth_service/auth_app/serializers.py
@@ -11,14 +11,22 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         token['username'] = user.username
-        logger.debug("Token created for user: %s", user.username)
+        if user.is_superuser:
+            token['role'] = 'superuser'
+        elif user.is_staff:
+            token['role'] = 'staff'
+        else:
+            token['role'] = 'active'
+        logger.debug("Token created for user: %s with role: %s", user.username, token['role'])
         return token
 
 
 class UserSerializer(serializers.ModelSerializer):
+    role = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'password']
+        fields = ['id', 'email', 'username', 'password', 'role']
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate_email(self, value):
@@ -28,11 +36,17 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         logger.info("Creating a new user with data: %s", validated_data)
+        role = validated_data.pop('role', 'active')
         user = User.objects.create_user(
             email=validated_data['email'],
             username=validated_data['username'],
             password=validated_data['password'],
         )
+        if role == 'staff':
+            user.is_staff = True
+        elif role == 'superuser':
+            user.is_superuser = True
+        user.save()
         logger.info("User created successfully with ID: %s", user.id)
         return user
 
