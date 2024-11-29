a76c9ab3b59280e5a015c6f7a7b4a9eef2037a81
Aleksei Konstantinov <mralexey111@gmail.com>
2024-11-29 14:35:08 +0300

    Change language of content to English

--- frontend_service/src/pages/AboutPage.js
+++ frontend_service/src/pages/AboutPage.js
@@ -3,15 +3,15 @@ import React from 'react';
 const AboutPage = () => {
     return (
         <div className="container mt-5">
-            <h2>О проекте</h2>
+            <h2>About the Project</h2>
             <p>
-                Этот проект создан для того, чтобы пользователи могли легко находить и бронировать отели по всему миру.
-                Мы стремимся сделать процесс бронирования простым, быстрым и удобным.
+                This project is designed to help users easily find and book hotels worldwide.
+                Our mission is to make the booking process simple, fast, and convenient.
             </p>
             <ul className="list-group mt-3">
-                <li className="list-group-item">Простой интерфейс</li>
-                <li className="list-group-item">Быстрый поиск</li>
-                <li className="list-group-item">Широкий выбор отелей</li>
+                <li className="list-group-item">User-friendly interface</li>
+                <li className="list-group-item">Quick search</li>
+                <li className="list-group-item">Wide range of hotels</li>
             </ul>
         </div>
     );
--- frontend_service/src/pages/HomePage.js
+++ frontend_service/src/pages/HomePage.js
@@ -3,14 +3,13 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div className="container text-center mt-5">
-            <h1 className="display-4">Добро пожаловать!</h1>
+            <h1 className="display-4">Welcome!</h1>
             <p className="lead">
-                Это веб-ресурс для бронирования отелей. Здесь вы сможете найти и забронировать лучшие предложения.
+                This is a hotel booking platform. Find and reserve the best deals right here.
             </p>
-            <button className="btn btn-primary btn-lg mt-3">Начать</button>
+            <button className="btn btn-primary btn-lg mt-3">Get Started</button>
         </div>
     );
 };
 
 export default HomePage;
-    
