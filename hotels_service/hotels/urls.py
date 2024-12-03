6724d0f02077cb3b09544d4eaea7ecbc749fd853
Aleksei Konstantinov <mralexey111@gmail.com>
2024-12-04 01:25:00 +0300

    Add search urls for hotels and rooms

--- hotels_service/hotels/urls.py
+++ hotels_service/hotels/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import HotelViewSet, HotelTypeViewSet, CurrentUserHotelsView, HotelImageUploadView, HotelImageDeleteView
+from .views import HotelViewSet, HotelTypeViewSet, CurrentUserHotelsView, HotelImageUploadView, HotelImageDeleteView, SearchHotelsAPIView
 
 router = DefaultRouter()
 router.register('types', HotelTypeViewSet, basename='hotel-type')
 router.register('hotels', HotelViewSet, basename='hotel')
 
 urlpatterns = [
-   path('user/', CurrentUserHotelsView.as_view(), name='current_user_hotels'),
-   path('hotels/<int:hotel_id>/upload-images/', HotelImageUploadView.as_view(), name='hotel_upload_images'),
-   path('hotels/<int:hotel_id>/delete-images/<int:image_id>/', HotelImageDeleteView.as_view(), name='hotel_delete_images'),
+   path('user/', CurrentUserHotelsView.as_view(), name='current-user-hotels'),
+   path('<int:hotel_id>/upload-images/', HotelImageUploadView.as_view(), name='hotel-upload-images'),
+   path('<int:hotel_id>/delete-images/<int:image_id>/', HotelImageDeleteView.as_view(), name='hotel-delete-images'),
+   path('search/', SearchHotelsAPIView.as_view(), name='search-hotels'),
    path('', include(router.urls)),
 ]
--- hotels_service/rooms/urls.py
+++ hotels_service/rooms/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RoomViewSet, RoomTypeViewSet, RoomImageUploadView, RoomImageDeleteView
+from .views import RoomViewSet, RoomTypeViewSet, RoomImageUploadView, RoomImageDeleteView, SearchRoomsAPIView, SearchRoomsByIdsView
 
 router = DefaultRouter()
 router.register('rooms', RoomViewSet, basename='room')
 router.register('types', RoomTypeViewSet, basename='room-type')
 
 urlpatterns = [
-   path('rooms/<int:room_id>/upload-images/', RoomImageUploadView.as_view(), name='room_upload_images'),
-   path('rooms/<int:room_id>/delete-images/<int:image_id>/', RoomImageDeleteView.as_view(), name='room_delete_images'),
+   path('rooms/<int:room_id>/upload-images/', RoomImageUploadView.as_view(), name='room-upload-images'),
+   path('rooms/<int:room_id>/delete-images/<int:image_id>/', RoomImageDeleteView.as_view(), name='room-delete-images'),
+   path('search/', SearchRoomsAPIView.as_view(), name='search-rooms'),
+   path('search-by-ids/', SearchRoomsByIdsView.as_view(), name='search-by-ids'),
    path('', include(router.urls)),
 ]
