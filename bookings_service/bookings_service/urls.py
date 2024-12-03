38c0d2334d0052ae00514c93c4f2a30a5589677b
Aleksei Konstantinov <mralexey111@gmail.com>
2024-12-04 01:11:01 +0300

    Add url path to bookings app

--- bookings_service/bookings_service/urls.py
+++ bookings_service/bookings_service/urls.py
@@ -2,8 +2,9 @@
 URL configuration for bookings_service project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/bookings/', include('bookings.urls')),
 ]
